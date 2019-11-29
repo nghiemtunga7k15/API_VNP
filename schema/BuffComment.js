b317ffa4f33647914114c3a3e65eab8aa645692a
Tung <nghiemtunga7k15@gmai.com>
2019-11-29 13:33:03 +0700

    Update Text

--- schema/BuffComment.js
+++ schema/BuffComment.js
@@ -8,7 +8,7 @@ var BuffCommentSchema =  mongoose.Schema({
     comments            :   { type: String , required: true },     
     comments_count      :   { type: Number , required: true },     
     total_price_pay     :   { type: Number , required: true },     
-    time_type           :   { type: Number },  // 0 là deyplay 1 là time buff done
+    time_type           :   { type: Number },  // 0 là delay 1 là time buff done
     time_value          :   { type: Number },
     note                :   { type: String, default : 'Descript' },
     status              :   { type: Number, default : 0 },
