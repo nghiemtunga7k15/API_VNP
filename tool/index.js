6d1f69337eff29c83e21a471db46acf317666009
Tung <nghiemtunga7k15@gmai.com>
2019-12-06 14:59:21 +0700

    Update TOOL ConverturltoID

--- tool/index.js
+++ tool/index.js
@@ -8,14 +8,19 @@ module.exports = {
 		const regex_5 = /permalink\.php\?story_fbid=(.*?)&id/;
 		const regex_6 = /posts\/(.*)/;
 		const regex_7 = /posts\/(.*?)\?__xts__/;
-
-		let matching  =   string.match(regex_1) ||  string.match(regex_2) || string.match(regex_3) || string.match(regex_4) || string.match(regex_5) || string.match(regex_7) || string.match(regex_6)
-
+		const regex_8 = /[0-9]{10,30}/;
+		let matching  =  string.match(regex_1) ||  string.match(regex_2) || string.match(regex_3) || string.match(regex_4) || string.match(regex_5) || string.match(regex_7)  || string.match(regex_6) ;
+		let matchID  = string.match(regex_8);
 		if ( matching ) {
 			matching.forEach((match, groupIndex) => {
 			    if(groupIndex > 0) {
 			    	result = match;
 		     	}	     
+			});
+		}
+		if (matchID) {
+			matchID.forEach((match, groupIndex) => {
+			    	result = match;     
 			});
 		}
 
